082e4281db615b5786351a91b04a947c822e17b2
玄枫若雲 <dhixuan@qq.com>
2024-11-06 23:24:05 +0800

    my code

---
+++ python_course_01.py
@@ -0,0 +1,6 @@
+# @Time    : 2024/11/5 01:01
+# @Author  : dhixuan
+# @File    : python_course_01.py
+# @Software: PyCharm
+
+print("hello, world")
---
+++ python_course_02.py
@@ -0,0 +1,6 @@
+# @Time    : 2024/11/5 00:59
+# @Author  : dhixuan
+# @File    : python_course_02.py
+# @Software: PyCharm
+
+print("hello world")
